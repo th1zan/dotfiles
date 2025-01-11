231608e3714504d0af08a4bee9369860d3c99d61
Thibault Sanglard <thibault@Thibaults-MacBook-Air.local>
2025-01-11 13:46:57 +0100

    chatGPT plugin added

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,40 @@
 
 -- open file_browser with the path of the current buffer
 vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+
+-- WhichKey plugin mappings for ChatGPT
+local wk = require("which-key")
+wk.add({
+  { "<leader>a", group = "ChatGPT", icon = "" }, -- Définit le groupe ChatGPT
+  { "<leader>ac", "<cmd>ChatGPT<CR>", desc = "ChatGPT", icon = "", mode = { "n", "v" } },
+  {
+    "<leader>ae",
+    "<cmd>ChatGPTEditWithInstruction<CR>",
+    desc = "Edit with instruction",
+    icon = "",
+    mode = { "n", "v" },
+  },
+  {
+    "<leader>ag",
+    "<cmd>ChatGPTRun grammar_correction<CR>",
+    desc = "Grammar Correction",
+    icon = "",
+    mode = { "n", "v" },
+  },
+  { "<leader>at", "<cmd>ChatGPTRun translate<CR>", desc = "Translate", icon = "", mode = { "n", "v" } },
+  { "<leader>ak", "<cmd>ChatGPTRun keywords<CR>", desc = "Keywords", icon = "", mode = { "n", "v" } },
+  { "<leader>ad", "<cmd>ChatGPTRun docstring<CR>", desc = "Docstring", icon = "", mode = { "n", "v" } },
+  { "<leader>aa", "<cmd>ChatGPTRun add_tests<CR>", desc = "Add Tests", icon = "", mode = { "n", "v" } },
+  { "<leader>ao", "<cmd>ChatGPTRun optimize_code<CR>", desc = "Optimize Code", icon = "", mode = { "n", "v" } },
+  { "<leader>as", "<cmd>ChatGPTRun summarize<CR>", desc = "Summarize", icon = "", mode = { "n", "v" } },
+  { "<leader>af", "<cmd>ChatGPTRun fix_bugs<CR>", desc = "Fix Bugs", icon = "", mode = { "n", "v" } },
+  { "<leader>ax", "<cmd>ChatGPTRun explain_code<CR>", desc = "Explain Code", icon = "", mode = { "n", "v" } },
+  { "<leader>ar", "<cmd>ChatGPTRun roxygen_edit<CR>", desc = "Roxygen Edit", icon = "", mode = { "n", "v" } },
+  {
+    "<leader>al",
+    "<cmd>ChatGPTRun code_readability_analysis<CR>",
+    desc = "Code Readability Analysis",
+    icon = "",
+    mode = { "n", "v" },
+  },
+})
--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -1,6 +1,28 @@
 return {
   "folke/which-key.nvim",
+  event = "VeryLazy",
   opts = {
     preset = "modern", -- Options disponibles : "classic", "modern", "helix"
   },
+  -- keys = {
+  --   { "<leader>a", group = "ChatGPT", name = "ChatGPT", icon = "🤖" },
+  --   { "<leader>ac", "<cmd>ChatGPT<CR>", desc = "ChatGPT" },
+  --   { "<leader>ae", "<cmd>ChatGPTEditWithInstruction<CR>", desc = "Edit with instruction", mode = { "n", "v" } },
+  --   { "<leader>ag", "<cmd>ChatGPTRun grammar_correction<CR>", desc = "Grammar Correction", mode = { "n", "v" } },
+  --   { "<leader>at", "<cmd>ChatGPTRun translate<CR>", desc = "Translate", mode = { "n", "v" } },
+  --   { "<leader>ak", "<cmd>ChatGPTRun keywords<CR>", desc = "Keywords", mode = { "n", "v" } },
+  --   { "<leader>ad", "<cmd>ChatGPTRun docstring<CR>", desc = "Docstring", mode = { "n", "v" } },
+  --   { "<leader>aa", "<cmd>ChatGPTRun add_tests<CR>", desc = "Add Tests", mode = { "n", "v" } },
+  --   { "<leader>ao", "<cmd>ChatGPTRun optimize_code<CR>", desc = "Optimize Code", mode = { "n", "v" } },
+  --   { "<leader>as", "<cmd>ChatGPTRun summarize<CR>", desc = "Summarize", mode = { "n", "v" } },
+  --   { "<leader>af", "<cmd>ChatGPTRun fix_bugs<CR>", desc = "Fix Bugs", mode = { "n", "v" } },
+  --   { "<leader>ax", "<cmd>ChatGPTRun explain_code<CR>", desc = "Explain Code", mode = { "n", "v" } },
+  --   { "<leader>ar", "<cmd>ChatGPTRun roxygen_edit<CR>", desc = "Roxygen Edit", mode = { "n", "v" } },
+  --   {
+  --     "<leader>al",
+  --     "<cmd>ChatGPTRun code_readability_analysis<CR>",
+  --     desc = "Code Readability Analysis",
+  --     mode = { "n", "v" },
+  --   },
+  -- },
 }
